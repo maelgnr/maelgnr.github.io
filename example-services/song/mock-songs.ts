5c1f54ce9420af78c9b7fecdb9f1a84a5158fa01
maelgnr <94389018+maelgnr@users.noreply.github.com>
2022-01-24 16:38:15 +0100

    Add files via upload

---
+++ example-services/song/mock-songs.ts
@@ -0,0 +1,14 @@
+export const SONGS: any[] =
+  [
+    {
+      year: 'Moi',
+      items: [
+        { link: '2Y6Nne8RvaA', top: 'Juillet-Août 2021', artist: 'CDD Technicien de laboratoire', title: 'CHLVO Challans (85)' , tache1: 'Etiquetage des échantillons', tache2: 'Enregistrement des analyses', tache3: 'Traitement pré-analytique des échantillons', tache4: 'Réalisation des analyses', competence1: 'Rigueur', competence2: 'Organisation', competence3: 'Adaptabilité', competence4: 'Connaissance en biologie'},
+        { link: 'b1_B-IKEufg', top: 'Mai-Juillet 2021', artist: 'Stage "Recrutement des bactéries du genre Zobellia dans les métagénomes marins"', title: 'Station Biologique de Roscoff (29)', tache1: 'Sélection des métagénomes d\'intérêt', tache2: 'Estimation de l\'abondance de Zobellia avec Kraken+Bracken', tache3: 'Assemblage des reads de Zobellia avec MetaspaDES' , tache4: 'Annotation des reads avec Prokka, Sulfatlas et Cupp', competence1: 'Programmation Bash', competence2: 'Autonomie', competence3: 'Communication', competence4: 'Workflow d\'analyse'},
+        { link: 'nYh-n7EOtMA', top: 'Juillet-Août 2020', artist: 'CDD Technicien de laboratoire', title: 'CHLVO Challans (85)', tache1: 'Etiquetage des échantillons', tache2: 'Enregistrement des analyses', tache3: 'Traitement pré-analytique des échantillons', tache4: 'Réalisation des analyses', competence1: 'Rigueur', competence2: 'Organisation', competence3: 'Adaptabilité', competence4: 'Dynamisme' },
+        { link: 'ru0K8uYEZWw', top: 'Juin 2019', artist: 'Stage d\'observation pôle recherche clinique', title: 'Institut de Cancérologie de l\'Ouest St-Herblain (44) ' , tache1: 'Observation du métier d\'ARC', tache2: 'Observation du fonctionnement des Essais Cliniques', tache3: 'Observation des études cliniques'},
+        { link: 'SxyahDOtqZw', top: 'Eté 2019 + Eté 2018', artist: 'CDD Maraichage', title: 'GAEC du Manoir de la Brosse, Fresnay (44)', tache1: 'Désherbage', tache2: 'Récolte Poireaux/Carottes/Céleri', tache3: 'Conditionnement', competence1: 'Dynamisme'},
+        { link: 'pXRviuL6vMY', top: 'Eté 2017', artist: 'CDD Maraichage', title: 'Maraicher GENDRON Bruno, Noirmoutier (85)', tache1: 'Récolte Fraise/Tomate/Pomme de terre ...', tache2: 'Vente' , competence1: 'Dynamisme', },
+      ]
+    },
+  ];
---
+++ example-services/song/song.ts
@@ -0,0 +1,32 @@
+export class Song {
+
+  link: string;
+  top: string;
+  artist: string;
+  title: string;
+  tache1: string;
+  tache2: string;
+  tache3: string;
+  tache4: string;
+  competence1: string;
+  competence2: string;
+  competence3: string;
+  competence4: string;
+
+  constructor() {
+    this.link = '';
+    this.top = '';
+    this.artist = '';
+    this.title = '';
+    this.tache1 = '';
+    this.tache2 = '';
+    this.tache3 = '';
+    this.tache4 = '';
+    this.competence1 = '';
+    this.competence2 = '';
+    this.competence3 = '';
+    this.competence4 = '';
+
+  }
+
+}
